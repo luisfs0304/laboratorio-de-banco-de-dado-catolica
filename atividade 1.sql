a6ca2118255b81c665e05fcbf1b04b6734531fa0
luisfs0304 <130685257+luisfs0304@users.noreply.github.com>
2026-03-11 21:15:03 -0300

    atividade 1.sql

--- atividade 1.sql
+++ atividade 1.sql
@@ -19,12 +19,12 @@ SHOW tables;
 
 create table livro (
 id_livro int auto_increment primary key,
-titulo varchar(255),
+titulo varchar(255)not null,
 sinopse text not null,
-autor varchar(128),
-editora varchar(128),
-categoria varchar(128),
-ano_publicacao year
+autor varchar(128)not null,
+editora varchar(128)not null,
+categoria varchar(128)not null,
+ano_publicacao year not null
 
 );
 
